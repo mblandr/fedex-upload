0f8c0b8adcd518761e47c7e21da365259effeb68
Lebedinsky Alexandr <mblandr@gmail.com>
2025-11-27 16:55:35 +0200

    s

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const server = Bun.serve({
 	port: 4000,
 	async fetch(req) {
 		const url = new URL(req.url);
-		console.log(url.pathname)
+		console.log(url.pathname);
 
 		if (url.pathname === '/') {
 			return new Response('bun is worked fine');
@@ -10,20 +10,20 @@ const server = Bun.serve({
 		// parse formdata at /action
 		if (url.pathname === '/upload') {
 			try {
-			const formdata = await req.formData();
-			const metadata = formdata.get('document');
-			console.log('document metadata', document);
-			const attachedDocument = formdata.get('attachment');
-			console.log('document file', attachedDocument);
-			const response = {
-				metadata,
-				attachment: `${attachedDocument}`,
-			};
-			return Response.json(JSON.stringify(response));
+				const formdata = await req.formData();
+				const metadata = formdata.get('document');
+				console.log('document metadata', document);
+				const attachedDocument = formdata.get('attachment');
+				console.log('document file', attachedDocument);
+				const response = {
+					metadata,
+					attachment: `${attachedDocument}`,
+				};
+				return Response.json(JSON.stringify(response));
 			} catch (e) {
-				return Response.json(JSON.stringify({error: `${e}`}));
+				console.log(e);
+				return Response.json(JSON.stringify({ error: `${e}` }));
 			}
-
 		}
 
 		return new Response('Not Found', { status: 404 });
